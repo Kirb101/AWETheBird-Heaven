7e48e1579f4a99d8d768f4d75d6b370d8526c315
AWEsome5199 <awhitcherestrada@hotmail.com>
2024-07-31 09:43:00 +0100

    hello

---
+++ AboutPage/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var backButton = document.querySelector(".back");
+    backButton.addEventListener("click", function() {
+        window.location.href = "..\\Homepage\\homepage.html";
+    });
+});
---
+++ script.js
@@ -0,0 +1,3 @@
+function goToPage2() {
+    window.location.href = 'HomePage/homepage.html';
+}
